959fef614f7ec427c667656000f1f34f7fa4a0f9
Alex Root <newrou@mail.ru>
2018-06-06 16:04:01 +0700

    Change MS images list

--- Diazo-MS-Integrate/make-index.sh
+++ Diazo-MS-Integrate/make-index.sh
@@ -2,8 +2,8 @@
 echo "<html>" > index-auto.html
 echo "<head><meta charset=\"utf-8\"/></head>" >> index-auto.html
 echo "<body>" >> index-auto.html
-echo "<h3>Спектры:</h3>" >> index-auto.html
-echo "<ul>" >> index-auto.html
+echo "<h2>Спектры:</h2>" >> index-auto.html
+#echo "<ul>" >> index-auto.html
 
 cd img
 list=`ls *.png`
@@ -11,9 +11,10 @@ for i in $list
 do
   name=`basename $i .png`
   ref="img/$name.png"
-  echo " <li> <a href=\"$ref\"> $name </a> </li>" >> ../index-auto.html
+  echo " <div><center><img src=\"$ref\" width=\"100%\"><br/> <h3>Fig. NN. ESI/MS of the $name in the positive mode.</h3><br/> </center><div>" >> ../index-auto.html
+#  echo " <li> <img src=\"$ref\"> <h3>$name</h3> </li>" >> ../index-auto.html
 done
 cd ..
-echo "</ul>" >> index-auto.html
+#echo "</ul>" >> index-auto.html
 echo "</body>" >> index-auto.html
 echo "</html>" >> index-auto.html
