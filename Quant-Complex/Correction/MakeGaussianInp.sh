8f771a7c2b123a8a05f56d02e625626b415b8c8b
Alex Root <newrou@mail.ru>
2019-03-16 17:10:35 +0700

    Quant correction

---
+++ Quant-Complex/Correction/MakeGaussianInp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+basis=$1
+
+mkdir -p inp-gaussian
+list=`ls *.mol`
+for i in $list
+do
+   Name=`basename $i .mol`
+   InName=`basename $i .mol`-$1-$2.gjf
+   echo "New file: "$Name
+   echo "%chk=$Name.chk" > $InName
+   cat head-gaussian | sed -e "s/METHOD/$1/" | sed -e "s/BASIS/$3/" >> $InName
+   echo "$Name-$1-$2" >> $InName
+   echo "" >> $InName
+   Charge="1 1"
+   if [[ $i =~ "-.mol" ]] ; then 
+      Charge="-1 1"
+   fi
+   if [[ $i =~ "+.mol" ]] ; then 
+      Charge="1 1"
+   fi
+   echo "Charge: [$Charge]"
+   echo "$Charge" >> $InName
+   babel -imol $i -ogjf - | tail -n +6 >> $InName
+   cat tail-gaussian >> $InName
+   mv $InName inp-gaussian
+done
